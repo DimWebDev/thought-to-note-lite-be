cbf385c2f887965230d7121bcdd48a0a4586ceb6
Dimitrios Liasis <your_email@example.com>
2024-08-10 09:47:13 +0300

    Add .gitignore entries for various IDEs, build tools, and OS-specific files; initialize Spring Boot project

---
+++ src/main/java/com/thoughttonotelite/ThoughtToNoteLiteBeApplication.java
@@ -0,0 +1,13 @@
+package com.thoughttonotelite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThoughtToNoteLiteBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThoughtToNoteLiteBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thoughttonotelite/ThoughtToNoteLiteBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thoughttonotelite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThoughtToNoteLiteBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
