ed8d7180d7193770efba1ef5c8164566b6b2aa10
Dimitrios Liasis <your_email@example.com>
2024-08-18 12:51:07 +0300

    enable CORS in the Backend to allow the FE part of this Application to interact with the Backend API

--- src/main/java/com/thoughttonotelite/config/SecurityConfig.java
+++ src/main/java/com/thoughttonotelite/config/SecurityConfig.java
@@ -7,6 +7,11 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -18,12 +23,25 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())  // Disable CSRF protection for simplicity in testing with Postman
+                .cors(withDefaults())  // Enable CORS with default settings
                 .authorizeHttpRequests(authz -> authz
                         .anyRequest().authenticated()  // Require authentication for all requests
                 )
                 .httpBasic(withDefaults());  // Enable basic authentication
 
         return http.build();
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.setAllowedOrigins(Arrays.asList("http://localhost:3000"));  // Allow your frontend origin
+        config.setAllowedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "Authorization"));
+        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
     }
 
     @Bean
